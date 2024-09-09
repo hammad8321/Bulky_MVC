9c1914ac213273a4c4fa3f040ab12af9cab690bf
Hammad Saleem Rana <HSRana@brandservices.com.au>
2024-09-09 16:38:35 +1000

    V30 Add Category Controller

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,12 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(o=>o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -22,6 +27,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
+    //   Controller/Action/Id
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
