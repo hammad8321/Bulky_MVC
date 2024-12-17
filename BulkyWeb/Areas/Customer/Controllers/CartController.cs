ac3184f1bc27a3db60bdcacfffb937fb92b605e6
HSRana <hsrana@brandservices.com.au>
2024-12-17 14:01:59 +1100

    V157. Order Placed Sucessfully with Stripe

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -219,66 +219,31 @@ namespace BulkyWeb.Areas.Customer.Controllers
         }
 
 
-        //[HttpPost]
-        //[ActionName("Summary")]
-        //public IActionResult SummaryPOST()
-        //{
-        //    var claimsIdentity = (ClaimsIdentity)User.Identity;
-        //    var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-        //    ShoppingCartVM.ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
-        //        includeProperties: "Product");
-
-        //    ShoppingCartVM.OrderHeader.OrderDate = System.DateTime.Now;
-        //    ShoppingCartVM.OrderHeader.ApplicationUserId   = userId;
-
-        //    ApplicationUser applicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
-
-
-        //    foreach (var cart in ShoppingCartVM.ShoppingCartList)
-        //    {
-        //        cart.Price = GetPriceBasedOnQuantity(cart);
-        //        ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
-        //    }
-
-        //    if (applicationUser.CompanyId.GetValueOrDefault() == 0)
-        //    {
-        //        //it is a regular customer 
-        //        ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
-        //        ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusPending;
-        //    }
-        //    else
-        //    {
-        //        //it is a company user
-        //        ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment;
-        //        ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusApproved;
-        //    }
-        //    _unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
-        //    _unitOfWork.Save();
-        //    foreach (var cart in ShoppingCartVM.ShoppingCartList)
-        //    {
-        //        OrderDetail orderDetail = new()
-        //        {
-        //            ProductId = cart.ProductId,
-        //            OrderHeaderId = ShoppingCartVM.OrderHeader.Id,
-        //            Price = cart.Price,
-        //            Count = cart.Count
-        //        };
-        //        _unitOfWork.OrderDetail.Add(orderDetail);
-        //        _unitOfWork.Save();
-        //    }
-
-        //    if (applicationUser.CompanyId.GetValueOrDefault() == 0)
-        //    {
-        //        //it is a regular customer account and we need to capture payment
-        //        //stripe logic
-        //    }
-
-        //    return RedirectToAction(nameof(OrderConfirmation), new { id = ShoppingCartVM.OrderHeader.Id });
-        //}
 
         public IActionResult OrderConfirmation(int id)
         {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.Get(a => a.Id == id, includeProperties: "ApplicationUser");
+            if (orderHeader.PaymentStatus != SD.PaymentStatusDelayedPayment)
+            {
+                // this Order by customer
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId);
+
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    _unitOfWork.OrderHeader.UpdateStripePaymentId(id, session.Id, session.PaymentIntentId);
+                    _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.StatusApproved);
+                    _unitOfWork.Save();
+                }
+
+            }
+            List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
+                .GetAll(x=>x.ApplicationUserId== orderHeader.ApplicationUserId).ToList();
+
+
+            _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+            _unitOfWork.Save();
             return View(id);
         }
 
