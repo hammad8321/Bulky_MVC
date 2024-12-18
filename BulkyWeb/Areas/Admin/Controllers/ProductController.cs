4e4be05a546c31504831e7698c83933893d6f5d1
HSRana <hsrana@brandservices.com.au>
2024-12-18 16:42:40 +1100

    V159.Order List UI (Completed)

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -107,65 +107,10 @@ namespace BulkyWeb.Areas.Admin.Controllers
             }
            
         }
-        //public IActionResult Edit(int? id)
-        //{
-        //    if (id == null || id == 0)
-        //    {
-        //        return NotFound();
-        //    }
-        //    // Category? categoryFromDb = _categoryRepo.Get(u => u.Id == id);
-        //    // Category? categoryFromDb = _categoryRepo.Categories.FirstOrDefault(c => c.Id == id);
-        //    // Category categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
-        //    Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-        //    if (productFromDb == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(productFromDb);
-        //}
 
-        //[HttpPost]
-        //public IActionResult Edit(Product o)
-        //{
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        _unitOfWork.Product.Update(o);
-        //        _unitOfWork.Save();
-        //        TempData["success"] = "Product Updated..!!";
-        //        return RedirectToAction("Index");
+      
 
-        //    }
-        //    return View();
-
-        //}
-
-        //public IActionResult Delete(int? id)
-        //{
-        //    if (id == null || id == 0)
-        //    {
-        //        return NotFound();
-        //    }
-        //    Product? productFromDb = _unitOfWork.Product.Get(c => c.Id == id);
-
-        //    if (productFromDb == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(productFromDb);
-        //}
-        //[HttpPost, ActionName("Delete")]
-        //public IActionResult DeletePOST(int? id)
-        //{
-        //    Product? o = _unitOfWork.Product.Get(u => u.Id == id);
-        //    if (o == null)
-        //    { return NotFound(); }
-        //    _unitOfWork.Product.Remove(o);
-        //    _unitOfWork.Save();
-        //    TempData["success"] = "Product bye bye..!!";
-        //    return RedirectToAction("Index");
-
-        //}
         #region API CALLS
 
         [HttpGet]
